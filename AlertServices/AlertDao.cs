1419ef4c64099c78acae32a7fafc4ba4f72060d0
Zuhair Haroon Khan <zk222ac@gmail.com>
2019-06-12 15:55:08 +0200

    final release

---
+++ AlertServices/AlertDao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlertServices
+{
+    // Task no:2 AlertDAO should implement the IAlertDAO interface.
+    public class AlertDao : IAlertDao
+    {
+        private readonly Dictionary<Guid, DateTime> _alerts = new Dictionary<Guid, DateTime>();
+
+        public Guid AddAlert(DateTime time)
+        {
+            Guid id = Guid.NewGuid();
+            _alerts.Add(id, time);
+            return id;
+        }
+
+        public DateTime GetAlert(Guid id)
+        {
+            return _alerts[id];
+        }
+
+        //Task 3: AlertService should have a constructor that accepts IAlertDAO.
+        public IAlertDao IAlertDao { get; set; }
+        public AlertDao( IAlertDao iAlertDao)
+        {
+            IAlertDao = iAlertDao;
+        }
+    }
+}
--- AlertServices/Program.cs
+++ AlertServices/Program.cs
@@ -10,12 +10,74 @@ namespace AlertServices
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            
+            Console.WriteLine("Alert Service!......");
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            IAlertDao iAlertDao = new AlertDao();
+            AlertService alertService = new AlertService(iAlertDao);
+           
+            // Raise Alert 
+            var id = alertService.RaiseAlert();
+            // Get Alert 
+            Console.WriteLine("alert Id" + id);
+
+            var date = alertService.GetAlertTime(id);
+            Console.WriteLine("alert date" + date);
+            Console.ReadKey();
+        }
+    }
+    public class AlertDao : IAlertDao
+    {
+        private readonly Dictionary<Guid, DateTime> _alerts = new Dictionary<Guid, DateTime>();
+
+        public Guid AddAlert(DateTime time)
+        {
+            Guid id = Guid.NewGuid();
+            _alerts.Add(id, time);
+            return id;
+        }
+
+        public DateTime GetAlert(Guid id)
+        {
+            if (_alerts.ContainsKey(id))
+            {
+                return _alerts[id];
+            }
+            else
+            {
+                Console.WriteLine("Key does not exist");
+            }
+            return  new DateTime();
+        }
+
+       
+    }
+
+    public interface IAlertDao
+    {
+        Guid AddAlert(DateTime time);
+        DateTime GetAlert(Guid id);
+
+    }
+    public class AlertService
+    {
+        //Task 3: AlertService should have a constructor that accepts IAlertDAO.
+        public IAlertDao AlertDao { get; set; }
+        public AlertService(IAlertDao iAlertDao)
+        {
+            //Dependency Injection here.....
+            AlertDao = iAlertDao;
+        }
+        
+        // The RaiseAlert and GetAlertTime methods should use the object passed through the constructor.
+        public Guid RaiseAlert()
+        {
+            return AlertDao.AddAlert(DateTime.Now);
+        }
+
+        public DateTime GetAlertTime(Guid id)
+        {
+            return AlertDao.GetAlert(id);
         }
     }
 }
